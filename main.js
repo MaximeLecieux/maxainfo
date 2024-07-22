0b97352a2c531552751bb421b473caa82e92bb61
Maxime <maxime.lecieuxlmf@gmail.com>
2024-07-22 10:14:25 +0200

    Cinfiguration de la messagerie

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+/*=============== SHOW MENU ===============*/
+
 const   navToggle = document.getElementById('nav-toggle')
         navClose = document.getElementById('nav-close')
         navMenu = document.getElementById('nav-menu')
@@ -16,3 +18,34 @@ if(navClose){
     })
 }
 
+/*=============== EMAIL JS ===============*/
+
+const contactForm = document.getElementById('contact-form'),
+      contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) =>{
+    e.preventDefault()
+
+    console.log(contactForm)
+
+    // serviceID - templateID - #form - publicKey
+    emailjs.sendForm('service_gbntvkl', 'template_ifie9g9', '#contact-form', 'jM5rmCxLkgb1d12Fl')
+    .then(() =>{
+        //show sent message
+        contactMessage.textContent = 'Message reçu 👍 Je vous répond dès que possible !'
+
+        // Remove message after five seconds
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+
+        }, 5000)
+
+        // Clear input fields
+        contactForm.reset()
+    }, () =>{
+        //show error message
+        contactMessage.textContent = 'Message non envoyé (service error) 👎'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
